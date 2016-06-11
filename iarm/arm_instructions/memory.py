abf2820b7347fa25016b7563fbbee8fe02fcc9c2
Joshua Milas <Josh.Milas@gmail.com>
2016-06-10 21:13:28 -0400

    Added all memory instruction and test stubs

--- iarm/arm_instructions/memory.py
+++ iarm/arm_instructions/memory.py
@@ -1,5 +1,44 @@
+import iarm.exceptions
 from ._meta import _Meta
 
 
 class Memory(_Meta):
-    pass
+    def ADR(self, params):
+        raise iarm.exceptions.NotImplementedError
+
+    def LDM(self, params):
+        raise iarm.exceptions.NotImplementedError
+
+    def LDR(self, params):
+        raise iarm.exceptions.NotImplementedError
+
+    def LDRB(self, params):
+        raise iarm.exceptions.NotImplementedError
+
+    def LDRH(self, params):
+        raise iarm.exceptions.NotImplementedError
+
+    def LDRSB(self, params):
+        raise iarm.exceptions.NotImplementedError
+
+    def LDRSH(self, params):
+        raise iarm.exceptions.NotImplementedError
+
+    def POP(self, params):
+        raise iarm.exceptions.NotImplementedError
+
+    def PUSH(self, params):
+        raise iarm.exceptions.NotImplementedError
+
+    def STM(self, params):
+        raise iarm.exceptions.NotImplementedError
+
+    def STR(self, params):
+        raise iarm.exceptions.NotImplementedError
+
+    def STRB(self, params):
+        raise iarm.exceptions.NotImplementedError
+
+    def STRH(self, params):
+        raise iarm.exceptions.NotImplementedError
+
--- tests/test_iarm.py
+++ tests/test_iarm.py
@@ -969,6 +969,59 @@ class TestArmShift(TestArm):
     def test_RORS(self):
         pass
 
+class TestArmMemory(TestArm):
+    @unittest.skip('No Test Defined')
+    def test_ADR(self):
+        pass
+
+    @unittest.skip('No Test Defined')
+    def test_LDM(self):
+        pass
+
+    @unittest.skip('No Test Defined')
+    def test_LDR(self):
+        pass
+
+    @unittest.skip('No Test Defined')
+    def test_LDRB(self):
+        pass
+
+    @unittest.skip('No Test Defined')
+    def test_LDRH(self):
+        pass
+
+    @unittest.skip('No Test Defined')
+    def test_LDRSB(self):
+        pass
+
+    @unittest.skip('No Test Defined')
+    def test_LDRSH(self):
+        pass
+
+    @unittest.skip('No Test Defined')
+    def test_POP(self):
+        pass
+
+    @unittest.skip('No Test Defined')
+    def test_PUSH(self):
+        pass
+
+    @unittest.skip('No Test Defined')
+    def test_STM(self):
+        pass
+
+    @unittest.skip('No Test Defined')
+    def test_STR(self):
+        pass
+
+    @unittest.skip('No Test Defined')
+    def test_STRB(self):
+        pass
+
+    @unittest.skip('No Test Defined')
+    def test_STRH(self):
+        pass
+
 
 if __name__ == '__main_':
     unittest.main()
